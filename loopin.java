f8291c08a81e34a0a834e8f663e4680e1764c92c
prokashghosh12930 <prokashghosh12930@gmail.com>
2023-03-23 22:40:33 +0530

    [refacto] code

--- loopin.java
+++
@@ -1,9 +0,0 @@
-public class loopin {
-    public static void main(String[] args) {
-        int i = 0;
-        while (i < 10) {
-            System.out.println("Value of I:" + i);
-            i++;
-        }
-    }
-}
