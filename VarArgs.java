f36446ee020c74be573a7f33f1eedaf1554a6752
prokashghosh12930 <prokashghosh12930@gmail.com>
2023-08-01 07:21:00 +0530

    factorIAL OF NUMBER

--- VarArgs.java
+++ VarArgs.java
@@ -7,9 +7,17 @@ public class VarArgs {
         return result;
     }
 
+    static int factoial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factoial(n - 1);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to variable argument;;");
         System.out.println("Print the sum of two numbers" + sumOf(3, 5, 6));
-
+        System.out.println("Factorial of number" + factoial(4));
     }
 }
